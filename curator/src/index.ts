a57aff4481914c8d27978d5168918ca41d7ebb57
Coleman Irby <ckirbyee92@gmail.com>
2026-02-12 23:18:43 -0600

    Missed in previous push

--- curator/src/index.ts
+++ curator/src/index.ts
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 
-app.get('/resource', (req, res) => {
+app.get('/games', (req, res) => {
   console.log("resource endpoint called");
-  res.status(402).json({ price: 9.99, owner: 'djmoneystax' });
+  res.status(402).json({ price: 69.99, owner: 'djmoneystax' });
+});
+
+app.get('/music', (req, res) => {
+  console.log("resource endpoint called");
+  res.status(402).json({ price: 420.99, owner: 'djmoneystax' });
 });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
