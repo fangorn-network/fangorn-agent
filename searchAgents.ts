5a763013edc8c797b1e75ce2fc7839cf2f62418e
Coleman Irby <ckirbyee92@gmail.com>
2026-02-28 15:09:32 -0600

    Update searchAgents.ts to include both overrides for arbitrum

--- searchAgents.ts
+++ searchAgents.ts
@@ -1,7 +1,11 @@
 import { SDK } from "agent0-sdk";
 
+// Arbitrum Sepolia
+const chainId = 421614;
+const rpcUrl = "https://sepolia-rollup.arbitrum.io/rpc";
+const subgraphUrl = "https://gateway.thegraph.com/api/4d3e61e064765d1001ee86baba46a907/subgraphs/id/6WuFQqo3FR5F76fCR4Bkfnymu64S5iu2tgX7JZsxQxg9";
 const registryOverrides = {
-  421614: {
+  chainId: {
     // Retrieved from https://github.com/erc-8004/erc-8004-contracts/blob/master/README.md
     IDENTITY: "0x8004A818BFB912233c491871b3d84c89A494BD9e",
     REPUTATION: "0x8004B663056A597Dffe9eCcC1965A193B7388713",
@@ -9,22 +13,36 @@ const registryOverrides = {
 };
 
 const subgraphOverrides = {
-  421614:
-    "https://api.studio.thegraph.com/query/1742225/erc-8004-arbitrum-sepolia/version/latest",
+  chainId: subgraphUrl,
 };
 
+// Arbitrum One
+// const chainId = 42161;
+// const rpcUrl = "https://arb1.arbitrum.io/rpc";
+// const subgraphUrl = "https://gateway.thegraph.com/api/61fa508eec436e052c9159527d4c8bb1/subgraphs/id/HZ6yKjjbYpkLTXLJBxfe4HWN3jxkLfLNJXh4zeVj1t9L";
+// const registryOverrides = {
+//   chainId: {
+//     // Retrieved from https://github.com/erc-8004/erc-8004-contracts/blob/master/README.md
+//     IDENTITY: "0x8004A169FB4a3325136EB29fA0ceB6D2e539a432",
+//     REPUTATION: "0x8004BAa17C55a88189AE136b182e5fdA19dE9b63",
+//   },
+// };
+
+// const subgraphOverrides = {
+//   chainId: subgraphUrl,
+// };
+
 const sdk = new SDK({
-  chainId: 421614,
-  rpcUrl: "https://sepolia-rollup.arbitrum.io/rpc",
-  subgraphUrl:
-    "https://api.studio.thegraph.com/query/1742225/erc-8004-arbitrum-sepolia/version/latest",
+  chainId,
+  rpcUrl,
+  subgraphUrl,
   registryOverrides,
   subgraphOverrides,
 });
 
-const results = await sdk.searchAgents({ chains: [421614] });
+const results = await sdk.searchAgents({ chains: [chainId] });
 console.log(
-  `Number of bots returned for chain Arbitrum Sepolia with chainId 421614 with MCP enabled is ${results.length} `,
+  `Number of agents returned for chain with chainId ${chainId} is ${results.length} `,
   results.length,
 );
 let ourAgent;
