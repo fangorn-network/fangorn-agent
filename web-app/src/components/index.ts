ec2b8ad79ed084fdec48869754ddcab25504d7f0
Coleman Irby <ckirbyee92@gmail.com>
2026-03-30 01:00:47 -0500

    Removed unused files

--- web-app/src/components/index.ts
+++ web-app/src/components/index.ts
@@ -1,4 +1,3 @@
-export { default as FangornUI } from "./FangornUI";
 export { DataRecordCard } from "./Cards/DataRecordCard";
 export { SchemaCard, SchemaDetailCard } from "./Cards/SchemaCards";
 export { Pill, Bubble, Card, FieldRow, TypingDots, ActionBtn, EncryptedBadge, Pagination, truncAddr } from "./primitives";
