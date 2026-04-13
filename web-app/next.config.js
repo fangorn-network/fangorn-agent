548679d1e14403c3ec4ad45cfc71ae41ca41f3d4
Coleman Irby <ckirbyee92@gmail.com>
2026-04-13 17:06:30 -0500

    WIP added upload view

--- web-app/next.config.js
+++ web-app/next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+	reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+			config.resolve.alias = {
+        ...config.resolve.alias,
+        '@farcaster/mini-app-solana': false,
+      };
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        path: false,
+        os: false,
+        crypto: false,
+        stream: false,
+      };
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
